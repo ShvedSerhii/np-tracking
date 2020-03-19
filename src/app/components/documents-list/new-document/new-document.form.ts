788d840a7b2e1321d57ad0cc96d1aaed2c0c1b27
Serhii <chuhaystyr@gmail.com>
2020-03-20 01:46:27 +0200

    edit document

--- src/app/components/documents-list/new-document/new-document.form.ts
+++ src/app/components/documents-list/new-document/new-document.form.ts
@@ -28,7 +28,7 @@ import { DocumentModel } from 'src/app/models/document.model';
         }),
         phone: new FormControl(this.model.Phone, {
           validators: [
-            Validators.pattern(/^(\+380)[0-9]{9}$/) 
+            Validators.pattern(/^(380)[0-9]{9}$/) 
           ],
           updateOn: 'change'
         })
