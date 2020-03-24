71d052ce8a2d201195b93f5bdc89a73e58f6dd67
Serhii <chuhaystyr@gmail.com>
2020-03-24 13:47:34 +0200

    add spinner

--- src/app/components/documents-list/documents-list.component.ts
+++ src/app/components/documents-list/documents-list.component.ts
@@ -7,6 +7,7 @@ import { getDocuments } from "src/app/store/selectors/documents.selectors";
 import * as DocumentsActions from "./../../store/actions/documents.actions";
 import { NewDocumentComponent } from './new-document/new-document.component';
 import { MatDialog } from '@angular/material/dialog';
+import { LoaderService } from 'src/app/services/loader/loader.service';
 
 @Component({
   selector: "app-documents-list",
@@ -15,9 +16,13 @@ import { MatDialog } from '@angular/material/dialog';
 })
 export class DocumentsListComponent {
   public documents$: Observable<Array<DocumentStatus>>;
-  constructor(private store: Store<IAppState>, public dialog: MatDialog) {
+  public loading: boolean;
+  constructor(private store: Store<IAppState>, public dialog: MatDialog, private loaderService: LoaderService) {
     this.store.dispatch(new DocumentsActions.GetDocuments());
     this.documents$ = this.store.select(getDocuments);
+    this.loaderService.isLoading.subscribe((v) => {
+      this.loading = v;
+    });
   }
 
   public openDialog(): void {
